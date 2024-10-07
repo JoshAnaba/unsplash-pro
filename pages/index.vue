00ed8d567fa73e2b7b16696ed9e38259d48e8289
joshanaba <anabajoshchi@gmail.com>
2024-10-07 03:27:57 +0100

    added meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -13,13 +13,11 @@ useHead({
 </script>
 
 <!-- TODO: Add page transitions -->
-<!-- TODO: Add page title -->
-<!-- TODO: Add page description -->
 <!-- TODO: infinite scroll for pagination -->
 <!-- TODO: route query params should match the API query params -->
 <!-- TODO: lazy loading where necessary -->
 <!-- TODO: check out for bugs -->
 <!-- TODO: add download buttons to images; top right corner -->
 <!-- TODO: go through the instructions for the last time -->
-<!-- Separate components where neccessary -->
-
+<!-- TODO:Separate components where neccessary -->
+<!-- TODO: hide CLient ID from public repo -->
