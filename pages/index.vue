fe27f4d0be50c4328382ee2a7ddf9fdff1406b91
joshanaba <anabajoshchi@gmail.com>
2024-10-07 22:23:57 +0100

    completed todos

--- pages/index.vue
+++ pages/index.vue
@@ -11,15 +11,6 @@ useHead({
   ],
 })
 </script>
-
-<!-- TODO: Add page transitions -->
-<!-- TODO: infinite scroll for pagination -->
-<!-- TODO: route query params should match the API query params -->
-<!-- TODO: lazy loading where necessary -->
 <!-- TODO: check out for bugs -->
-<!-- TODO: add download buttons to images; top right corner -->
 <!-- TODO: go through the instructions for the last time -->
-<!-- TODO:Separate components where neccessary -->
-<!-- TODO: hide CLient ID from public repo -->
-<!-- TODO: add readme.md, check out requirements and see if any applies to completed test, like ci.yml, Gulp -->
 <!-- TODO: cater for 404 search page -->
