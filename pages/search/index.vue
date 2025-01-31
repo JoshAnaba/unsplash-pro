d46cfbcfe3da1e00a25053755f7e7eb45b2ef9d4
joshanaba <anabajoshchi@gmail.com>
2025-01-31 19:49:38 +0100

    .

---
+++ pages/search/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: `Search`,
+  meta: [
+    {
+      name: 'description',
+      content: 'This is the search page description.',
+    },
+    {
+      property: 'og:title',
+      content: `Search`,
+    },
+  ],
+});
+</script>
