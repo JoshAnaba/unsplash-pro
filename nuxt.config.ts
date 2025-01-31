af556ce74ce287052e735109c57274b7f769c789
joshanaba <anabajoshchi@gmail.com>
2025-01-31 11:19:03 +0100

    testing meta title handling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Photo Library by Jon Doe',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'A photo library viewing and sharing photos' },
+        { property: 'og:title', content: 'Photo Library by Jon Doe' },
+        { property: 'og:description', content: 'A photo library viewing and sharing photos' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   ssr: false,
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
