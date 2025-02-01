bebb3f1d0e100e799b57eb1db5a86dfad2ff1c4b
joshanaba <anabajoshchi@gmail.com>
2025-02-01 15:24:22 +0100

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Photo Library by Jon Doe',
+      // title: 'Photo Library by Jon Doe',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { property: 'og:title', content: 'Photo Library by Jon Doe' },
-        { property: 'og:description', content: 'A photo library viewing and sharing photos' },
+        // { property: 'og:title', content: 'Photo Library by Jon Doe' },
+        // { property: 'og:description', content: 'A photo library viewing and sharing photos' },
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
